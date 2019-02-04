184c81cb59c106988cb781913f8c5770722f47de
navike21 <navike21@gmail.com>
2019-02-04 08:40:15 -0500

    upload data

--- js/ready.js
+++ js/ready.js
@@ -6,4 +6,32 @@ $(document).ready( ()=>{
     pause: 9000,
     speed: 1000
   });
+
+  $('a[href*="#"]').on('click', function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+  });
+
+  $(document).scroll(function () {
+    var y = $(this).scrollTop();
+    var heightScreen = parseInt($(window).height()) - ($(window).height() / parseInt(2));
+    if(y > 100) {
+      $('.header').addClass('whiteHeader');
+      $('.logo_activo').addClass('activeUp');
+    }
+    else{
+      $('.header').removeClass('whiteHeader');
+      $('.logo_activo').removeClass('activeUp');
+    }
+    
+    if (y > heightScreen) {
+      $('.upArrow').addClass('activeUp');
+    } else {
+      $('.upArrow').removeClass('activeUp');
+    }
+  });
+
 })
